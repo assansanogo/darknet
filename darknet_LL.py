bb101d687a4e91202949d0355230c20136a6984d
Assan Sanogo <assansanogo@gmail.com>
2021-10-11 01:38:14 +0200

    yolo lambda

--- darknet_LL.py
+++ darknet_LL.py
@@ -257,7 +257,7 @@ def yolo_liberta_leasing_convert_handler(event, context):
     formatting of the lambda handler to be compatible with by AWS
     '''
     # information extracted from the event payload
-    event = json.loads(base64.b64decode(event['body']).decode('utf-8'))
+    #event = json.loads(base64.b64decode(event['body']).decode('utf-8'))
     input_file_url = event["url"]
     output_format = event["format"]
     
